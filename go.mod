5db6ef97d2cf1c9c2542cac5dda78c80c47e7a8e
aaronb12-ux <afairster03@gmail.com>
2025-12-04 19:52:44 -0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module aaron.com/todocli
+
+go 1.23.4
+
+require github.com/cristalhq/acmd v0.12.0 // indirect
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	
+	"fmt"
+
+
+	"github.com/cristalhq/acmd"
+)
+
+func home() {
+
+	fmt.Println("\nWelcome to the TODO task manager!\n")
+	fmt.Println("Your following options are:\n")
+	fmt.Println("Add Task")
+	fmt.Println("View Tasks")
+	fmt.Println("Mark Task as Completed")
+	fmt.Println("Type 'help' for a basic rundown of the tool")
+}
+
+func main() {
+
+	cmds := []acmd.Command{
+	{
+		Name:        "todo",
+		Description: "shows home screen",
+		ExecFunc: func(ctx context.Context, args []string) error {
+			home()
+			return nil
+		},
+	},
+}
+
+r := acmd.RunnerOf(cmds, acmd.Config{
+})
+
+if err := r.Run(); err != nil {
+	fmt.Println(err)
+	r.Exit(err)
+}
+}
